609ad23f4390dfe40fe83b5702a7f3ae41105ce0
aliyev002 <168084470+aliyev002@users.noreply.github.com>
2024-10-21 22:09:45 +0400

    Update Program.cs

--- TcpServer/Program.cs
+++ TcpServer/Program.cs
@@ -21,13 +21,16 @@ while (true)
         int fileNameLength = stream.Read(fileNameBytes, 0, fileNameBytes.Length);
         var fileName = Encoding.UTF8.GetString(fileNameBytes, 0, fileNameLength);
 
+
+
+
+        
+
         
         var fileLengthBytes = new byte[1024]; 
         int fileLengthLength = stream.Read(fileLengthBytes, 0, fileLengthBytes.Length);
         var fileLength = long.Parse(Encoding.UTF8.GetString(fileLengthBytes, 0, fileLengthLength));
-
         var path = $"{DateTime.Now:HH.mm.ss}_{fileName}";
-
         
         using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
         {
