395dcfb562203711e9ab66361885723b36bf66c8
Togrul <togrul.kbtrl05@gmail.com>
2024-10-21 22:07:35 +0400

    first commit

---
+++ TcpClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+var serverAddress = "26.230.244.228"; 
+var port = 27001; 
+
+var path = @"C:\Users\Legion\Desktop\sinaq\koala.jpg";
+
+try
+{
+    FileInfo file = new FileInfo(path);
+    TcpClient client = new TcpClient(serverAddress, port);
+    using (NetworkStream stream = client.GetStream())
+    {
+        
+        var fileNameBytes = Encoding.UTF8.GetBytes(file.Name);
+        stream.Write(fileNameBytes, 0, fileNameBytes.Length); 
+
+        
+        var fileLengthBytes = Encoding.UTF8.GetBytes(file.Length.ToString());
+        stream.Write(fileLengthBytes, 0, fileLengthBytes.Length); 
+
+        
+        using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+        {
+            var buffer = new byte[5000];
+            int bytesRead;
+            while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                stream.Write(buffer, 0, bytesRead);
+            }
+        }
+        Console.WriteLine("Fayl gnoderildi.");
+    }
+    client.Close();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Xeta bash verdi: {ex.Message}");
+}
---
+++ TcpServer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var localAddress = IPAddress.Any;
+var port = 27001;
+
+TcpListener listener = new TcpListener(localAddress, port);
+listener.Start();
+Console.WriteLine("Server isleyir...");
+
+while (true)
+{
+    using (TcpClient client = listener.AcceptTcpClient())
+    using (NetworkStream stream = client.GetStream())
+    {
+        
+        var fileNameBytes = new byte[1024]; 
+        int fileNameLength = stream.Read(fileNameBytes, 0, fileNameBytes.Length);
+        var fileName = Encoding.UTF8.GetString(fileNameBytes, 0, fileNameLength);
+
+        
+        var fileLengthBytes = new byte[1024]; 
+        int fileLengthLength = stream.Read(fileLengthBytes, 0, fileLengthBytes.Length);
+        var fileLength = long.Parse(Encoding.UTF8.GetString(fileLengthBytes, 0, fileLengthLength));
+
+        var path = $"{DateTime.Now:HH.mm.ss}_{fileName}";
+
+        
+        using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+        {
+            var buffer = new byte[5000];
+            int bytesRead;
+            long totalReceived = 0;
+            while (totalReceived < fileLength && (bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                fs.Write(buffer, 0, bytesRead);
+                totalReceived += bytesRead;
+            }
+        }
+        Console.WriteLine($"Fayl qebul edildi və saxlanildi: {path}");
+    }
+}
